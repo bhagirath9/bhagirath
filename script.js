ae231792d42764382906914118e039fbd0097526
bhagirath9 <bhakarbhagirath62@gmail.com>
2023-10-29 22:32:23 +0530

    add my project

---
+++ script.js
@@ -0,0 +1,15 @@
+//console.log("script running...")
+ /*document.querySelector('.cross') .style.display = 'none'*/
+//document.querySelector('.hamburger').addEventListener("click",()=>{
+//    document.querySelector('.sidebar').classList.toggle('sidebarGo');
+//    if(document.querySelector('.sidebar').classList.contains('sidebarGo')){
+  //      document.querySelector('.ham').Style.display='none'
+      /*  document.querySelector('.cross').Style.display = 'inline'*/
+  //  }
+  /*  else{
+        document.querySelector('.ham').Style.display='inline'
+        document.querySelector('.cross').Style.display = 'none' 
+       
+    }*/
+//})
+
